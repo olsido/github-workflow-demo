a34bc61f2f55e394c7127163a5f75d0e76a83e80
Olga Strijewski <Olga.Strijewski@smithcommerce.com>
2024-07-03 17:01:33 -0400

    Project initial commit

---
+++ src/main/java/com/example/githubworkflowdemo/GithubWorkflowDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.githubworkflowdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubWorkflowDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubWorkflowDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/githubworkflowdemo/GithubWorkflowDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githubworkflowdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubWorkflowDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
