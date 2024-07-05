49c89f8787f255960c28694d5841620faa438671
Olga Strijewski <Olga.Strijewski@smithcommerce.com>
2024-07-05 15:23:35 -0400

    Added a simple controller plus test

--- src/main/java/com/example/githubworkflowdemo/GithubWorkflowDemoApplication.java
+++ src/main/java/com/example/githubworkflowdemo/GithubWorkflowDemoApplication.java
@@ -2,12 +2,22 @@ package com.example.githubworkflowdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class GithubWorkflowDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GithubWorkflowDemoApplication.class, args);
+    }
+
+    @RestController
+    class HelloWorldController {
+        @GetMapping("/")
+        public String hello() {
+            return "Hello, World!";
+        }
     }
 
 }
--- src/test/java/com/example/githubworkflowdemo/GithubWorkflowDemoApplicationTests.java
+++ src/test/java/com/example/githubworkflowdemo/GithubWorkflowDemoApplicationTests.java
@@ -3,11 +3,22 @@ package com.example.githubworkflowdemo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 class GithubWorkflowDemoApplicationTests {
 
     @Test
     void contextLoads() {
+        GithubWorkflowDemoApplication.HelloWorldController application = new GithubWorkflowDemoApplication().new HelloWorldController();
+        assertThat(application).isNotNull();
+    }
+
+    @Test
+    void testHelloWorld() {
+        GithubWorkflowDemoApplication.HelloWorldController controller = new GithubWorkflowDemoApplication().new HelloWorldController();
+        String response = controller.hello();
+        assertThat(response).isEqualTo("Hello, World!");
     }
 
 }
