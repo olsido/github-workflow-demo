6ef73e34e90a1b7016646c019d581f1c6064fc34
Olga Strijewski <Olga.Strijewski@smithcommerce.com>
2024-07-05 17:49:48 -0400

    Generate Sonar issues

--- src/main/java/com/example/githubworkflowdemo/GithubWorkflowDemoApplication.java
+++ src/main/java/com/example/githubworkflowdemo/GithubWorkflowDemoApplication.java
@@ -16,12 +16,21 @@ public class GithubWorkflowDemoApplication {
     class HelloWorldController {
         @GetMapping("/")
         public String hello() {
-            generateSonarIssue();
+            generateSonarIssue1();
+            generateSonarIssue2();
+            generateSonarIssue3();
+            generateSonarIssue4();
+            generateSonarIssue5();
+            generateSonarIssue6();
+            generateSonarIssue7();
+            generateSonarIssue8();
+            generateSonarIssue9();
+            generateSonarIssue10();
             return "Hello, World!";
         }
     }
 
-    private void generateSonarIssue() {
+    private void generateSonarIssue1() {
         try {
             int divideByZero = 1 / 0; // This will cause an exception
         } catch (Exception e) {
@@ -29,4 +38,75 @@ public class GithubWorkflowDemoApplication {
         }
     }
 
+    private void generateSonarIssue2() {
+        try {
+            int divideByZero = 1 / 0; // This will cause an exception
+        } catch (Exception e) {
+            // Empty catch block is a common issue that SonarCloud will detect
+        }
+    }
+
+    private void generateSonarIssue3() {
+        try {
+            int divideByZero = 1 / 0; // This will cause an exception
+        } catch (Exception e) {
+            // Empty catch block is a common issue that SonarCloud will detect
+        }
+    }
+
+    private void generateSonarIssue4() {
+        try {
+            int divideByZero = 1 / 0; // This will cause an exception
+        } catch (Exception e) {
+            // Empty catch block is a common issue that SonarCloud will detect
+        }
+    }
+
+    private void generateSonarIssue5() {
+        try {
+            int divideByZero = 1 / 0; // This will cause an exception
+        } catch (Exception e) {
+            // Empty catch block is a common issue that SonarCloud will detect
+        }
+    }
+
+    private void generateSonarIssue6() {
+        try {
+            int divideByZero = 1 / 0; // This will cause an exception
+        } catch (Exception e) {
+            // Empty catch block is a common issue that SonarCloud will detect
+        }
+    }
+
+    private void generateSonarIssue7() {
+        try {
+            int divideByZero = 1 / 0; // This will cause an exception
+        } catch (Exception e) {
+            // Empty catch block is a common issue that SonarCloud will detect
+        }
+    }
+
+    private void generateSonarIssue8() {
+        try {
+            int divideByZero = 1 / 0; // This will cause an exception
+        } catch (Exception e) {
+            // Empty catch block is a common issue that SonarCloud will detect
+        }
+    }
+
+    private void generateSonarIssue9() {
+        try {
+            int divideByZero = 1 / 0; // This will cause an exception
+        } catch (Exception e) {
+            // Empty catch block is a common issue that SonarCloud will detect
+        }
+    }
+
+    private void generateSonarIssue10() {
+        try {
+            int divideByZero = 1 / 0; // This will cause an exception
+        } catch (Exception e) {
+            // Empty catch block is a common issue that SonarCloud will detect
+        }
+    }
 }
